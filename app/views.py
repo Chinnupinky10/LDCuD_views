5842d936ac4db7bc82f2627a54f3c66596f34424
Chinnupinky10 <thummalapravallika4@gmail.>
2024-02-14 16:11:45 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from app.models import *
+
+from django.urls import reverse_lazy
 
 class SchoolList(ListView):
     model=School
@@ -25,6 +27,10 @@ class SchoolUpdate(UpdateView):
     model=School
     fields='__all__'
 
+class SchoolDelete(DeleteView):
+    model=School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
 
 
 
@@ -45,8 +51,54 @@ class SchoolUpdate(UpdateView):
     
 
 
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from app.models import *
+
+from django.urls import reverse_lazy
+
+class SchoolList(ListView):
+    model=School
+    context_object_name='schools'
+    ordering=['sname']
+    #queryset=School.objects.filter(id=1)
+    #template_name='app/school_list.html'
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sclobject'
+
+class SchoolCreate(CreateView):
+    model=School
+    fields='__all__'
+
+
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+
+class SchoolDelete(DeleteView):
+    model=School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
